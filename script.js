002fd76382b3a93ef3e11a4100cca147112b8677
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-07-18 15:54:53 -0600

    fixed bug with maps and sets

--- script.js
+++ script.js
@@ -158,8 +158,9 @@ currencies.forEach(function (value, key, map) {
 });
 
 //SET
+// Set
 const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
 console.log(currenciesUnique);
 currenciesUnique.forEach(function (value, _, map) {
-  console.log(`${key}: ${value}`);
+  console.log(`${value}: ${value}`);
 });
