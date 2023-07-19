a5ffdc678eb241a17d008bf2d06f4852a8bd9fcc
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-07-19 14:42:12 -0600

    filter method

--- script.js
+++ script.js
@@ -261,3 +261,18 @@ TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
 //   )}`;
 // });
 // console.log(movementsDescriptions);
+
+/*======= THE FILTER METHOD =======*/
+
+// const deposits = movements.filter(function (mov, i, arr) {
+//   return mov > 0;
+// });
+// console.log(movements);
+// console.log(deposits);
+
+// const depositsFor = [];
+// for (const mov of movements) if (mov > 0) depositsFor.push(mov);
+// console.log(depositsFor);
+
+// const withdrawals = movements.filer(mov => mov < 0);
+// console.log(withdrawals);
