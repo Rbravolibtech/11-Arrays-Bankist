3da7cc12039fbcaba244c08cc0c5d25fe925a336
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-07-25 12:08:57 -0600

    163 sorting arrays js

--- script.js
+++ script.js
@@ -61,11 +61,12 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovements = function (movements) {
+const displayMovements = function (movements, sort = false) {
   containerMovements.innerHTML = '';
-  //.textContent = 0
 
-  movements.forEach(function (mov, i) {
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const html = `
@@ -219,6 +220,14 @@ btnClose.addEventListener('click', function (e) {
   }
 
   inputCloseUsername.value = inputClosePin.value = '';
+});
+
+let sorted = false;
+
+btnSort.addEventListener('click', function (e) {
+  e.preventDefault();
+  displayMovements(currentAccount.movements, !sorted);
+  sorted = !sorted;
 });
 /*====================================LECTURES================================*/
 
@@ -547,3 +556,36 @@ const overBalance2 = accounts
   .flatMap(acc => acc.movements)
   .reduce((acc, mov) => acc + mov, 0);
 console.log(overBalance);
+
+/*======= SORTING ARRAYS =======*/
+
+const owners = ['Jonas', 'zach', 'Adam', 'Martha'];
+console.log(owners.sort());
+console.log(owners);
+
+//NUMBERS
+
+console.log(movements);
+console.log(movements.sort());
+
+//RETURN < 0, A, B
+//RETURN > 0, B, A
+
+//ASCENDING
+// movements.sort((a, b) => {
+//   if (a > b) return 1;
+//   if (b > a) return -1;
+// });
+
+movements.sort((a, b) => a - b);
+
+console.log(movements);
+
+// DESCENDING
+// movements.sort((a, b) => {
+//   if (a > b) return -1;
+//   if (a > b) return 1;
+// });
+
+movements.sort((a, b) => b - a);
+console.log(movements);
