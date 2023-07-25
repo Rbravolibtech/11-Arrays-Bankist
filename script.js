7c98e0baa24e1d5f8d86d8d5d99df388b2cb2321
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-07-25 11:44:16 -0600

    162 flat map js

--- script.js
+++ script.js
@@ -517,3 +517,33 @@ const deposit = mov => mov > 0;
 console.log(movements.some(deposit));
 console.log(movements.every(deposit));
 console.log(movements.filter(deposit));
+
+/*=======FLAT IN TO GET STARTED =======*/
+
+const arr = [[1, 2, 3], 4, 5, 6, 7, 8];
+console.log(arr.flat());
+
+const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
+console.log(arrDeep.flat(2));
+
+// 👇🏻 THIS CODE CAN BE TYPED AND WILL RESULT THE SAME AS BOTTOM EXAMPLE
+
+// const accountMovements = accounts.map(acc => acc.movements);
+// console.log(accountMovements);
+// const allMovements = accountMovements.flat();
+// console.log(allMovements);
+// const overBalance = allMovements.reduce((acc, mov) => acc + mov, 0);
+
+//THIS CODE IS THE SAME RESULT AS THE TOP ONE ☝🏻 //FLAT
+const overBalance = accounts
+  .map(acc => acc.movements)
+  .flat()
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overBalance);
+
+//FLAT MAP
+
+const overBalance2 = accounts
+  .flatMap(acc => acc.movements)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overBalance);
